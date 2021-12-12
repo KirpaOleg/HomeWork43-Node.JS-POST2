bbdf1356da04e1dbbc89cf12c0faf7547efed0e3
KirpaOleg <profix.ok@gmail.com>
2021-12-12 20:31:46 +0200

    all

--- routes/index.js
+++ routes/index.js
@@ -14,24 +14,68 @@ const getcountryProperties = async(url) => {
     const languages = Object.values(country.languages);
     const currencies = Object.keys(country.currencies)[0];
     const flag = country.flags.png;
+    const cat = [];
     // console.log('TEST2 >>>>', name);
-    return{name, population, languages, currencies, flag,}
+    return{name, population, languages, currencies, flag, cat,}
   });
-  console.log('TEST3 >>>>', countryProperties);
+  // console.log('TEST3 >>>>', countryProperties);
+  return countryProperties;
 }
 
-const countryPropertiesHTML = async(element, res) => {
-	return res.send(`<div class="counrty">
-                          <p>${element.name}</p><br>
-                          <div class="flag">
-                            <img src= ${element.flag}><br>
-                          </div>
-                          <div class="text">
-                            <p>Population: ${element.population}</p>
-                            <p>Languages: ${element.languages}</p>
-                            <p>Currencies: ${element.currencies}</p>
-                          </div>
-                        </div>`)
+const getCatsInfo = async() => {
+  const tempArr = await axios.get('https://api.thecatapi.com/v1/breeds');
+  const catArr = tempArr.data.map((catObj) => {
+    return {
+      coutryName: catObj.origin,
+      catImgUrl: (!catObj.image) ? null : catObj.image.url,
+      breed: catObj.name,
+    }
+  });
+  // console.log('TEST6 >>>>', catArr);
+  return catArr;
+}
+
+const concatArr = (country, cat) => {
+  country.forEach(countryElement =>{
+    cat.forEach(catElement => {
+      if(countryElement.name === catElement.coutryName){
+        countryElement.cat.push(catElement)
+      } 
+    })
+  })
+  return country;
+}
+
+
+const genCardsHTML = (element) => {
+  // console.log('TEST6 >>>>', element);
+  let card = '';
+  element.forEach(item => {
+    const countryHTML = `
+    <div class="counrty">
+      <p>${item.name}</p><br>
+      <div class="flag">
+        <img src= ${item.flag}><br>
+      </div>
+      <div class="text">
+        <p>Population: ${item.population}</p>
+        <p>Languages: ${item.languages}</p>
+        <p>Currencies: ${item.currencies}</p>
+      </div>
+    </div><br>`
+    const catsHTML = item.cat.map((item) => {
+      return `	          
+      <div class="cat">   
+        <img class="cat-img" src= ${item.catImgUrl}>       
+        <p class="cat-name">${item.breed}</p>
+			</div>`
+    })
+    card = `${card}<div class="card">${countryHTML}
+                     <div class="cat-card">${catsHTML}
+                     </div>
+                   </div>`
+  })
+  return card
 }
 
 /* GET home page. */
@@ -42,9 +86,15 @@ router.get('/', (req, res, next) => {
 router.post('/', upload.none(), async(req, res, next) => {
   const URL = `https://restcountries.com/v3.1/region/${req.body.region}`;  
   // console.log('TEST0 >>>>', URL);
-  const countryArr = await getcountryProperties(URL);
-  console.log('TEST4 >>>>', countryArr);
- 
+  const countrysArray = await getcountryProperties(URL);
+  // console.log('TEST4 >>>>', countryArray);
+  const catsArray = await getCatsInfo();
+  // console.log('TEST8 >>>>', catsArray);
+  const countrysAndCatsArray = await concatArr(countrysArray, catsArray)
+  // console.log('TEST9 >>>>', catsArray);
+  const cardsHTML = await genCardsHTML(countrysAndCatsArray);
+  // console.log('TEST6 >>>>', cardsHTML);
+  res.send(cardsHTML)
 });
 
 module.exports = router;
